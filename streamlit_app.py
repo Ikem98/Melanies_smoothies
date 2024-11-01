ebb28baad13b8a08fc7d113444c5ec974adbe02f
Sam <109921144+Ikem98@users.noreply.github.com>
2024-11-01 15:11:32 -0600

    Update streamlit_app.py
    Changed arrangement of the cnx.session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
-# Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
-    """
-)
-# session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+    """)
 cnx = st.connection("snowflake")
 session = cnx.session()
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
 
 
 name_on_order = st.text_input('Name on Smoothie')
@@ -25,18 +22,9 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-# # columns = my_dataframe.columns
-# # st.write(columns)
 
 
-# fruit_names_df = my_dataframe.select("FRUIT_NAME").to_pandas()
 
-# fruit_names = fruit_names_df["FRUIT_NAME"].tolist()
-
-# fruit_names = [''] + fruit_names
-
-# option = st.selectbox("What is your favoirite fruit?", my_dataframe,)
-# 
 
 if ingredients_list:
    
